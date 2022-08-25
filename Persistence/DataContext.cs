f144ccbc53a5d8915698986b7ce8324d68729ffb
Ayush Vyas <sidharatvyas26@gmail.com>
2022-08-25 21:34:44 +0530

    End of Section 16

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -13,23 +13,24 @@ namespace Persistence
         public DataContext(DbContextOptions options) : base(options)
         {
         }
-       public DbSet<Activity> Activities{get;set;}
-       public DbSet<ActivityAttendee> ActivityAttendees { get; set; }
+        public DbSet<Activity> Activities { get; set; }
+        public DbSet<ActivityAttendee> ActivityAttendees { get; set; }
+        public DbSet<Photo> Photos { get; set; }
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
-            builder.Entity<ActivityAttendee>(x=>x.HasKey(aa=>new {aa.AppUserId,aa.ActivityId}));
-           
-            builder.Entity<ActivityAttendee>()
-            .HasOne(u=>u.AppUser)
-            .WithMany(u=>u.Activities)
-            .HasForeignKey(u=>u.AppUserId);
+            builder.Entity<ActivityAttendee>(x => x.HasKey(aa => new { aa.AppUserId, aa.ActivityId }));
 
             builder.Entity<ActivityAttendee>()
-            .HasOne(u=>u.Activity)
-            .WithMany(u=>u.Attendees)
-            .HasForeignKey(u=>u.ActivityId);
+            .HasOne(u => u.AppUser)
+            .WithMany(u => u.Activities)
+            .HasForeignKey(u => u.AppUserId);
+
+            builder.Entity<ActivityAttendee>()
+            .HasOne(u => u.Activity)
+            .WithMany(u => u.Attendees)
+            .HasForeignKey(u => u.ActivityId);
         }
     }
 }
